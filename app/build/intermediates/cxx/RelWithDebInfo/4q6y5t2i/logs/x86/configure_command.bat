9f65f14ba05235072be363b87d1e18ca4ac28462
Drago <hitmanagent404@gmail.com>
2024-09-21 12:19:09 +0530

    Add files via upload

---
+++ app/build/intermediates/cxx/RelWithDebInfo/4q6y5t2i/logs/x86/configure_command.bat
@@ -0,0 +1,18 @@
+@echo off
+"C:\\Users\\mante\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\mante\\OneDrive\\Desktop\\Workspace\\ArduinoControllerApp\\AndroidStudioProjects\\app\\src\\main\\cpp" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=29" ^
+  "-DANDROID_PLATFORM=android-29" ^
+  "-DANDROID_ABI=x86" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_NDK=C:\\Users\\mante\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\mante\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\mante\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\mante\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\mante\\OneDrive\\Desktop\\Workspace\\ArduinoControllerApp\\AndroidStudioProjects\\app\\build\\intermediates\\cxx\\RelWithDebInfo\\4q6y5t2i\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\mante\\OneDrive\\Desktop\\Workspace\\ArduinoControllerApp\\AndroidStudioProjects\\app\\build\\intermediates\\cxx\\RelWithDebInfo\\4q6y5t2i\\obj\\x86" ^
+  "-DCMAKE_BUILD_TYPE=RelWithDebInfo" ^
+  "-BC:\\Users\\mante\\OneDrive\\Desktop\\Workspace\\ArduinoControllerApp\\AndroidStudioProjects\\app\\.cxx\\RelWithDebInfo\\4q6y5t2i\\x86" ^
+  -GNinja
