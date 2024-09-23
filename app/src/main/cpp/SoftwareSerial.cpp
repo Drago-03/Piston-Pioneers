d8cbdea6c04d09a00b0bc69ef062f4feb80be5d0
Drago s <mantejarora.com>
2024-09-23 23:54:10 +0530

    HI fellas

---
+++ app/src/main/cpp/SoftwareSerial.cpp
@@ -0,0 +1,13 @@
+//
+// Created by mante on 20-09-2024.
+//
+
+#include "SoftwareSerial.h"
+
+void SoftwareSerial::begin(int i) {
+
+}
+
+SoftwareSerial::SoftwareSerial(int i, int i1) {
+
+}
---
+++ app/src/main/cpp/SoftwareSerial.h
@@ -0,0 +1,18 @@
+//
+// Created by mante on 20-09-2024.
+//
+
+#ifndef ARDUINOCONTROLLERAPP_SOFTWARESERIAL_H
+#define ARDUINOCONTROLLERAPP_SOFTWARESERIAL_H
+
+
+class SoftwareSerial {
+
+public:
+    SoftwareSerial(int i, int i1);
+
+    void begin(int i);
+};
+
+
+#endif //ARDUINOCONTROLLERAPP_SOFTWARESERIAL_H
